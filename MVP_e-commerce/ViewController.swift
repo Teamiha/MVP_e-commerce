2626a078b3b47e5cb86da11fb65dd16faa08fd7d
Mikhail Svetlov <jegnum@gmail.com>
2022-11-05 22:52:56 +0300

    Initial Commit

---
+++ MVP_e-commerce/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVP_e-commerce
+//
+//  Created by Михаил Светлов on 05.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MVP_e-commerceUITests/MVP_e_commerceUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MVP_e_commerceUITestsLaunchTests.swift
+//  MVP_e-commerceUITests
+//
+//  Created by Михаил Светлов on 05.11.2022.
+//
+
+import XCTest
+
+final class MVP_e_commerceUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
